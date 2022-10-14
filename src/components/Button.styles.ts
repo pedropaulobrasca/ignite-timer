704472a767ff6e4b6064ff3bc358522f0a67b4c3
Pedro Brasca <pedro.brasca@tinnova.com.br>
2022-10-13 23:51:06 -0300

    feat: Configurando temas

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -16,8 +16,11 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 50px;
 
+  background-color: ${props => props.theme.primary};
+  color: ${props => props.theme.black};
+
   /* background-color: ${({ variant }) => buttonVariants[variant]} */
-  ${({ variant }) => css`
+  /* ${({ variant }) => css`
     background-color: ${buttonVariants[variant]};
-  `}
+  `} */
 `
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,5 @@
+export const defaultTheme = {
+  primary: '#82e756',
+  secundary: 'orange',
+  black: '#000',
+}
