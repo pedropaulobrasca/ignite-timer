92178a5ca31a2de035ced3225577a48096a4c05c
Pedro Brasca <pedro.brasca@tinnova.com.br>
2022-10-14 00:18:25 -0300

    Configurando ESLint

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,15 +1,8 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 import { ButtonVariant } from './Button'
 
 interface ButtonContainerProps {
   variant: ButtonVariant
-}
-
-const buttonVariants = {
-  primary: 'blue',
-  secondary: 'yellow',
-  danger: 'red',
-  success: 'green',
 }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
