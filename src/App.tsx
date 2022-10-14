d27a6cacc767ed9f509b00729d0ae42cbfa0276f
Pedro Brasca <pedro.brasca@tinnova.com.br>
2022-10-14 00:26:57 -0300

    React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
 import { defaultTheme } from './styles/themes/default'
+import { BrowserRouter } from 'react-router-dom'
 
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-      <Button />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
