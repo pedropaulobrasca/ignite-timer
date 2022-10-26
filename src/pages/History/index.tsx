44b00658c4b32563573005955566efecfaa5e757
Pedro Brasca <pedro.brasca@tinnova.com.br>
2022-10-25 21:19:37 -0300

    Componente: Status

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from './styles'
+import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
   return (
@@ -20,31 +20,25 @@ export function History() {
               <td>Estudar React</td>
               <td>20 minutos</td>
               <td>Há 10:00 horas</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
             </tr>
             <tr>
               <td>Estudar React</td>
               <td>20 minutos</td>
               <td>Há 10:00 horas</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
             </tr>
             <tr>
               <td>Estudar React</td>
               <td>20 minutos</td>
               <td>Há 10:00 horas</td>
-              <td>Concluído</td>
-            </tr>
-            <tr>
-              <td>Estudar React</td>
-              <td>20 minutos</td>
-              <td>Há 10:00 horas</td>
-              <td>Concluído</td>
-            </tr>
-            <tr>
-              <td>Estudar React</td>
-              <td>20 minutos</td>
-              <td>Há 10:00 horas</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
             </tr>
           </tbody>
         </table>
